05c051d13482e7f3b990a8db40135dbce6ee5356
root <root@ip-172-31-23-161.ec2.internal>
2024-05-19 13:55:58 +0000

    newcode

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0e001c9271cf7f3b9" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
+}
